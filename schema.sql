c18b535c5816223cf96605e679533c2e17a7d829
White Lily <mmacalino03@gmail.com>
2018-09-08 10:28:46 -0400

    touchup on mysql coding

---
+++ schema.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE Bamazon;
+
+USE Bamazon;
+
+CREATE TABLE Products(
+    ItemID MEDIUMINT AUTO_INCREMENT NOT NULL,
+    ProductName VARCHAR(100) NOT NULL,
+    DepartmentName VARCHAR(50) NOT NULL,
+    Price DECIMAL(10,2) NOT NULL,
+    StockQuantity INT(10) NOT NULL,
+    primary key(ItemID)
+);
+
+select * from Products;
+
+INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Kingdom Hearts 3","ENTERTAINMENT",49.95,150),
+    ("Breath of the Wild","ENTERTAINMENT",59.99,200),
+    ("Crate of Gochujang","GROCERY",24.50,50),
+    ("Air Jordans 10","CLOTHING",75.00,5),
+    ("Bomber Jacket","CLOTHING",54.25,35),
+    ("Fire Starter Kit","SPORTS & OUTDOORS",42.42,42),
+    ("Spirited Away","ENTERTAINMENT",15.00,25),
+    ("Game of Thrones Complete Series","ENTERTAINMENT",85.50,57),
+    ("Catan","ENTERTAINMENT",30.50,35),
+    ("Cards Against Humanity","ENTERTAINMENT",15.00,23);
+
+CREATE TABLE Departments(
+    DepartmentID MEDIUMINT AUTO_INCREMENT NOT NULL,
+    DepartmentName VARCHAR(50) NOT NULL,
+    OverHeadCosts DECIMAL(10,2) NOT NULL,
+    TotalSales DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY(DepartmentID));
+
+INSERT INTO Departments(DepartmentName, OverHeadCosts, TotalSales)
+VALUES ('ENTERTAINMENT', 50000.00, 15000.00),
+    ('ELECTRONICS', 20000.00, 12000.00),
+    ('HOME', 30000.00, 15000.00),
+    ('BODY & HEALTH', 3000.00, 12000.00),
+    ('GROCERY', 1200.00, 15000.00),
+    ('KIDS', 40000.00, 12000.00),
+    ('CLOTHING', 35000.00, 15000.00),
+    ('SPORTS & OUTDOORS', 12000.00, 12000.00);
+
